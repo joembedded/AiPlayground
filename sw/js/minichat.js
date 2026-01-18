966347a0ef66caaeeece8121571dbb5e0d8ee520
Jo <joembedded@gmail.com>
2026-01-18 16:21:05 +0100

    v0.07

--- sw/js/minichat.js
+++ sw/js/minichat.js
@@ -20,7 +20,7 @@
 //--------- globals ------ 
 import * as I18 from './intmain_i18n.js'
 
-export const VERSION = 'V0.06 / 17.01.2026';
+export const VERSION = 'V0.07 / 19.01.2026';
 export const COPYRIGHT = '(C) JoEmbedded.de';
 let dbgLevel = 1;   // 0: Kein Debug, 1: Meta-Daten, 2: Terminal, 3: Terminal+Micro nur abspielen, sonst nix
 
@@ -63,12 +63,14 @@ function terminalPrint(txt = '\u2424') { // NL-Symbol
     terminalEl.scrollTop = terminalEl.scrollHeight;
 }
 
-// Remove all emojis, clip at '<', normalize whitespace
+// Remove all emojis, clip at '<', normalize whitespace, Compact Strings
 function purifyText(text) {
     return text
-        .replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{FE00}-\u{FE0F}\u{1F1E0}-\u{1F1FF}]/gu, '')
+        .replace(/[\u{1F000}-\u{1FAFF}\u{2600}-\u{27BF}\u{2B00}-\u{2BFF}\u{E0000}-\u{E007F}\u{FE00}-\u{FE0F}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FAFF}\u{200D}\u{20E3}\u{FE0E}\u{FE0F}]/gu, '')
+        .replace(/[\u{1F1E0}-\u{1F1FF}]{2}/gu, '')
         .split('<')[0]
         .replace(/\s+/g, ' ')
+        .replace(/\s+([.,!:])/g, '$1')
         .trim();
 }
 
@@ -411,7 +413,7 @@ async function speakText(rawInputText, cache = false) {
     if (isProcessing) {
         return;
     }
-    const inputTextTrimmed = purifyText(rawInputText.trim());
+    const inputTextTrimmed = purifyText(rawInputText);
     if (inputTextTrimmed.length === 0) {
         dbgPrint('Kein Text eingegeben!');
         return;
